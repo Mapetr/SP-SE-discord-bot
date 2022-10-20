fef92a43a7e3c7d752ad517cb267a204290ad90c
Petr Mácha <info@mapetr.moe>
2022-10-20 19:21:28 +0200

    Rework emojiUpdate.ts

--- src/events/emojiUpdate.ts
+++ src/events/emojiUpdate.ts
@@ -8,14 +8,22 @@ export default {
     const embed = new EmbedBuilder()
       .setColor(0xffff00)
       .setTitle("Upraveno emoji")
-      .addFields(
-        { name: 'Název', value: newEmoji.name ?? "N/A" },
-        { name: 'Autor', value: newEmoji.author?.username ?? "N/A", inline: true },
-        { name: 'Animovaný', value: newEmoji.animated ? "Ano" : "Ne", inline: true },
-      )
+      .setURL(newEmoji.url)
       .setImage(newEmoji.url)
-      .setFooter({ text: `ID: ${newEmoji.id}` })
+      .setFooter({text: `ID: ${newEmoji.id}`})
       .setTimestamp();
+    // Available
+    if (oldEmoji.available !== newEmoji.available) {
+      embed.addFields({name: 'Dostupné', value: `${oldEmoji.available} -> ${newEmoji.available}`});
+    }
+    // Name
+    if (oldEmoji.name !== newEmoji.name) {
+      embed.addFields({name: 'Název', value: `${oldEmoji.name} -> ${newEmoji.name}`});
+    }
+    // Roles
+    if (oldEmoji.roles.cache.size !== newEmoji.roles.cache.size) {
+      embed.addFields({name: 'Role', value: `${oldEmoji.roles.cache.size} -> ${newEmoji.roles.cache.size}`});
+    }
     const sendChannel = newEmoji.guild.channels.cache.get(data.channel) as TextChannel;
     if (sendChannel) await sendChannel.send({embeds: [embed]});
     else console.error("Events: Channel is non-existent");
