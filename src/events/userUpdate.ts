ccaf576a8fc41b4ef4f412fbe2e43ee604de4470
Petr Mácha <info@mapetr.moe>
2022-10-21 21:17:52 +0200

    rework userUpdate.ts

--- src/events/userUpdate.ts
+++ src/events/userUpdate.ts
@@ -7,16 +7,28 @@ export default {
   async execute(oldUser: User, newUser: User) {
     const embed = new EmbedBuilder()
       .setColor(0xffff00)
-      .setTitle("Změněna nálepka")
-      .setFields(
-        { name: "Název", value: newUser.username }
-      )
-      .setFooter({ text: `ID: ${newUser.id}` })
+      .setTitle("Uživatel upraven")
+      .setImage(newUser.avatarURL())
+      .setFooter({text: `ID: ${newUser.id}`})
       .setTimestamp();
+    // Tag
+    if (oldUser.tag !== newUser.tag) {
+      embed.addFields({name: 'Tag', value: `${oldUser.tag} -> ${newUser.tag}`});
+    }
+    // Username
+    if (oldUser.username !== newUser.username) {
+      embed.addFields({name: 'Uživatelské jméno', value: `${oldUser.username} -> ${newUser.username}`});
+    }
+    // Discriminator
+    if (oldUser.discriminator !== newUser.discriminator) {
+      embed.addFields({name: 'Discriminator', value: `${oldUser.discriminator} -> ${newUser.discriminator}`});
+    }
+    // Avatar
+    if (oldUser.avatar !== newUser.avatar) {
+      embed.addFields({name: 'Avatar', value: `${oldUser.avatarURL()} -> ${newUser.avatarURL()}`});
+    }
     const sendChannel = newUser.client.channels.cache.get(data.channel) as TextChannel;
     if (sendChannel) await sendChannel.send({embeds: [embed]});
     else console.error("Events: Channel is non-existent");
   }
 }
-
-// TODO: Check for updated properties
