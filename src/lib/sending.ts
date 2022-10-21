e65a1d82275e947b4d78e4dfa8bb678121e61fa3
Petr Mácha <info@mapetr.moe>
2022-10-21 22:11:30 +0200

    return empty field embeds

--- src/lib/sending.ts
+++ src/lib/sending.ts
@@ -3,6 +3,7 @@ import {Client, EmbedBuilder, TextChannel} from "discord.js";
 import {captureException} from "@sentry/node";
 
 export async function sendLog(embed: EmbedBuilder, client: Client) {
+  if (embed.data.fields?.length === 0) return;
   const sendChannel = client.channels.cache.get(data.channel) as TextChannel;
   if (sendChannel) await sendChannel.send({embeds: [embed]});
   else captureException("Events: Channel is non-existent");
